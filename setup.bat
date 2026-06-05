bc4c234e16d0ac48ec5c4b7ce7a5bb1d1edd2da9
Maki Nishi <tantamoraru49@gmail.com>
2026-06-05 10:11:03 +0300

    how to setup

---
+++ setup.bat
@@ -0,0 +1,90 @@
+echo The QuashProc Copyright
+md C:\Users\crist\downloads\QuashProc
+md C:\Users\crist\downloads\QuashProc\libavdemux
+md C:\Users\crist\downloads\QuashProc\libavfilter
+cd C:\Users\crist\downloads\QuashProc\libavdemux
+cd C:\Users\crist\downloads\QuashProc\libavdemux > bin.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > mp4.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > csas.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > gff.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > src.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > tmp.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > gls.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > glsl.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > ffmpeg64.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > ffmpeg32.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > qt64.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > qt32.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > bitrate.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > codec.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > cginc.c
+cd C:\Users\crist\downloads\QuashProc\libavdemux > un.h
+cd C:\Users\crist\downloads\QuashProc\libavdemux > cs.h
+cd C:\Users\crist\downloads\QuashProc\libavdemux > qd.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter
+md C:\Users\crist\downloads\QuashProc\libavfilter\color
+md C:\Users\crist\downloads\QuashProc\libavfilter\distort
+md C:\Users\crist\downloads\QuashProc\libavfilter\generate
+md C:\Users\crist\downloads\QuashProc\libavfilter\other
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > hue.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > hue.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > huesaturation.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > huesaturation.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > curves.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > curves.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > negate.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > negate.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > invertrgba.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > invertrgba.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > colorchannelmixer.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > colorchannelmixer.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > grayscale.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > grayscale.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > lightrays.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > lightrays.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > rays.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > rays.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > cc.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\color > cc.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > kaleido.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > kaleido.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > zigzag.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > zigzag.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > polar.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > polar.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > geq.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > geq.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > twirl.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > twirl.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > twist.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > twist.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > shear.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > shear.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > vortex.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > vortex.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > wave.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > wave.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > mirorr.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > mirorr.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > uleiwave.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > uleiwave.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\distort > cgingtwirl.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > noise.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > colorbars.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > sierpinski.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > mandelbrot.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > julia.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > blockrainbownoise.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > ceatest.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\generate > raysgen.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > r.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > r.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > warhol.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > warhol.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > rubberband.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > rubberband.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > setpts.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > setpts.h
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > atempo.c
+cd C:\Users\crist\downloads\QuashProc\libavfilter\other > atempo.h
